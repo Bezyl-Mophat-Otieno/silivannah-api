3505cc7c69bc7af8dfb7580f464ffc2244b4990a
Bezyl Mophat Otieno <bezylmophatotieno@gmail.com>
2025-07-10 07:07:12 +0300

    Recreated the index.html file + added a different slot  name 'staging'

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 router.get('/', function(req, res, next) {
-  res.status(200).json({message: 'Welcome to azure api'});
+  res.status(200).json({message:`Welcome to a deployed version of the silivannah api in azure.  We're  running on port ${process.env.PORT} ${process.env.CUSTOM_PORT}`});
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
-
 /* GET users listing. */
-router.get('/', function(req, res, next) { 
+router.get('/health', function(req, res, next) { 
   res.status(200).json([{userName: 'Bezyl  Mophat Otieno'}]); 
 });
 
