d868548dc4b8565daee58da4d23dfd9e584cbe7a
Bezyl Mophat Otieno <bezylmophatotieno@gmail.com>
2025-07-10 07:34:47 +0300

    UI/UX changes on the staging environment

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 /* GET users listing. */
-router.get('/health', function(req, res, next) { 
+router.get('/', function(req, res, next) { 
   res.status(200).json([{userName: 'Bezyl  Mophat Otieno'}]); 
 });
 
