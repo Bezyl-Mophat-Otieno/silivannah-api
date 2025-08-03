4fa56b05aebd125348b8f35a06d5b24583856ffc
Bezyl Mophat Otieno <bezylmophatotieno@gmail.com>
2025-08-03 20:22:38 +0300

    Json being returned when you hit /users GET

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,13 @@ const express = require('express');
 const router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) { 
-  res.status(200).json([{userName: 'Bezyl  Mophat Otieno'}]); 
+  const me = {
+    name: "Bezyl Mophat Otieno",
+    userName: "Papa OJ",
+    age:30,
+    password: "dhsf-duih-jfh9-oejd-dkdj-8882-yehu"
+  }
+  res.status(200).json([me]); 
 });
 
 module.exports = router;
